a376353d9090da6de78e759ad76cb341d8b7df86
ElArahnid <eloigim@gmail.com>
2022-09-17 22:10:06 +0300

    timer

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ const optimizeNumber = (n) => (n + "").length === 2 ? str : "0" + n;
 
 const data = [];
 const colors = ["#660709aa", "#121896aa", "#E89F03aa", "#FFE490aa", "#209ECFaa" ];
-let n = getNumber(41, 1);
+let n = Math.floor(getNumber(61, 6) / 6) * 6;
 
 while(n--) {
     data.push(getNumber(101))
@@ -12,7 +12,7 @@ while(n--) {
 console.log(data);
 
 const box = document.querySelector(".box");
-
+box.innerHTML = "<div class=\"card\"></div>";
 for (let val of data) {
     box.innerHTML += `<div 
     class="card" 
@@ -20,3 +20,22 @@ for (let val of data) {
     ${val}
     </div>`;
 }
+
+
+function showTime(day = new Date) {
+let now = new Date();
+let final = new Date(2023, 1, 7);
+let d, h, m, s;
+let time = final - now;
+m = Math.floor(time / 60);
+s = time - m * 60;
+h = Math.floor(m / 60);
+m -= h * 60;
+d = Math.floor(h / 24);
+h -= d * 24;
+return `${d}д ${h}ч ${m}мин ${s}сек`;
+}
+
+setInterval(function() {
+    box.firstElementChild.innerText = `До окончания осталось ${showTime()}`
+}, 1000)
